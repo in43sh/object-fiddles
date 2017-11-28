1540fee2156b599c0e9f8c643540b7f7bcb5e25b
Sergey Sherstobitov <in43sh@gmail.com>
2017-11-28 16:39:16 -0700

    initial commit

--- practice.js
+++ practice.js
@@ -6,43 +6,33 @@
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
-
-
-
-
+var me = {
+  name: "Sergey",
+  age: 22
+}
+window.alert(me.name)
 
 
 //NEXT PROBLEM
 
-
-
-
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
-  //Code here
-
-
-
-
-
-
+var favoriteThings = {
+  band: "band",
+  food: "food",
+  person: "person",
+  book: "book",
+  movie: "movie",
+  holiday: "holiday"
+}
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
-
-  //Code here
-
-
-
-
+favoriteThings.car = "camry"
+favoriteThings.brand = "nike"
 
 //Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
-
-  //Code here
-
-
-
-
+favoriteThings.food = 'Chicken Nuggets'
+favoriteThings.book = 'Harry Potter'
 
 //NEXT PROBLEM
 
@@ -58,18 +48,13 @@ Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. 
 */
 
-  //Code here
-
-
-
-
-
+var backPack = {}
+var item = 'firstPocket'
+backPack[item] = 'chapstick'
 
 //After you do the above, alert your entire backPack object.
 
-  //Code here
-
- 
+window.alert(backPack)
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -77,16 +62,10 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-  //Code here
-
-
-
+console.log(backPack)
 
 
 //NEXT PROBLEM
-
-
-
 
 var user2 = {
         name: 'Ty',
@@ -99,10 +78,8 @@ var user2 = {
 //Let's say I, the user, decided to change my name and email address to the following
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
-  //Code Here
-
-
-
+user2.name = 'Tyler S. McGinnis'
+user2.email = 'tyler.mcginnis@devmounta.in'
 
 
 // =============================================
@@ -120,11 +97,7 @@ var user2 = {
 
 
 //Create an empty object called methodCollection.
-
-  //Code Here
-
-
-
+var methodCollection = {}
 
 /*
 Now add two methods (functions that are properties on objects) to your methodCollection
@@ -132,45 +105,37 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
  which logs 'hello' to the console. 
  */
 
-  //Code Here
-
-
-
-
+methodCollection['alertHello'] = function () {window.alert ('hello')}
+methodCollection['logHello'] = function () {console.log ('hello')}
 
 //Now call your alertHello and logHello methods.
-
-  //Code Here
-
-
-
-
-
-
+methodCollection['alertHello']
+methodCollection['logHello']
 
 
 //NEXT PROBLEM
-
 
 
 // Create a function called makePerson which takes in name, birthday, ssn as its
 // parameters and returns a new object with all of the information that you passed in.
-
-  //Code Here
-
-
-
-
-
-  
+function makePerson (name, birthday, ssn) {
+  return person = {
+    name: name,
+    birthday: birthday,
+    ssn: ssn
+  }
+}
 
 
 //NEXT PROBLEM
 
-
-
 // Create a function called makeCard which takes in cardNumber, expirationDate, and securityCode to make a Credit Card object and returns that object so that whenever you invoke makeCard, you get a brand new credit card.
 
-  //Code Here
-
-
+ function makeCard (cardNumber, expirationDate, securityCode) {
+  var creditCard = {
+    cardNumber: cardNumber,
+    expirationDate: expirationDate,
+    securityCode: securityCode
+  }
+  return creditCard
+ }
